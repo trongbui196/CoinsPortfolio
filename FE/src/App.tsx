23c5ef90122aca6a43d01e17f0f66f98c8827967
Trong Bui <tofn001@gmail.com>
2024-11-26 02:26:50 +0700

    tempo remove watchlistpage from app.tsx for vercel deploy test

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -2,9 +2,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { SiteHeader } from "../components/site-header";
 import CoinPage from "../Pages/CoinDetailPage";
 import MarketPage from "../Pages/MarketPage";
-import WatchlistPage from "../Pages/WatchlistPage";
 import PortfolioPage from "../Pages/PortfolioPage";
-
+//import WatchlistPage from "../Pages/Watchlistpage";
 function App() {
   return (
     <Router>
@@ -14,7 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<MarketPage />} />
           <Route path="/coin/:symbol" element={<CoinPage />} />
-          <Route path="/watchlist" element={<WatchlistPage />} />
+          {/* <Route path="/watchlist" element={<WatchlistPage />} /> */}
           <Route path="/portfolio" element={<PortfolioPage />} />
         </Routes>
       </div>
