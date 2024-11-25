f70c86f8ae0825629ce9ba75851f5d68506b9b71
trongbui196 <39149120+trongbui196@users.noreply.github.com>
2024-11-26 02:45:03 +0700

    Update App.tsx

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { SiteHeader } from "../components/site-header";
 import CoinPage from "../Pages/CoinDetailPage";
 import MarketPage from "../Pages/MarketPage";
-import WatchlistPage from "../Pages/WatchlistPage";
+//import WatchlistPage from "../Pages/WatchlistPage";
 import PortfolioPage from "../Pages/PortfolioPage";
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<MarketPage />} />
           <Route path="/coin/:symbol" element={<CoinPage />} />
-          <Route path="/watchlist" element={<WatchlistPage />} />
+          {/* <Route path="/watchlist" element={<WatchlistPage />} /> */}
           <Route path="/portfolio" element={<PortfolioPage />} />
         </Routes>
       </div>
