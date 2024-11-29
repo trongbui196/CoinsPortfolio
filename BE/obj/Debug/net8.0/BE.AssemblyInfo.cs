48428d83185c810de43f4e763b7122b60a95e3b2
Trong Bui <tofn001@gmail.com>
2024-11-30 00:54:43 +0700

    check

--- BE/obj/Debug/net8.0/BE.AssemblyInfo.cs
+++ BE/obj/Debug/net8.0/BE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f8bffac55a6c82c144cf58a075676d3e052aad2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bc6e91e1e77ed228c8ad4390adb5689f91346e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
