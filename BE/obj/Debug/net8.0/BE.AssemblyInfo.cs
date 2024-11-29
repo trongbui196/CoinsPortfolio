a9b3c5baa9d7e80a193dd354ed69c079ac4b63df
Trong Bui <tofn001@gmail.com>
2024-11-30 02:37:44 +0700

    vercel url2

--- BE/obj/Debug/net8.0/BE.AssemblyInfo.cs
+++ BE/obj/Debug/net8.0/BE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5187ad676b5ab00af9ba9d471f4a91cdb86c6b06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07967d393b0b0cb3eed7ed3047f359a782b0ccaa")]
 [assembly: System.Reflection.AssemblyProductAttribute("BE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
