f0ee357d5bd25bfd8cea65682361e4570d3138f6
trongbui196 <tofn001@gmail.com>
2024-12-17 17:50:35 +0700

    fix coin detail

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { SiteHeader } from "../components/site-header";
 import CoinPage from "../Pages/CoinDetailPage";
 import MarketPage from "../Pages/MarketPage";
-import React from "react"
 import LandingPage from "../Pages/LandingPage";
 import WListPage from "../Pages/WListPage";
 import PortfolioPage from "../Pages/PortfolioPage";
